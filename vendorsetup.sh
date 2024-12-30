23846e1d0253f303779236336529ac2a3036c614
gentleranger <gentlerangers@gmail.com>
2024-12-30 19:09:51 +0530

    spes: Cleanup vendorsetup.sh
    - Device specific files should be handled by local_manifests

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -16,12 +16,3 @@ rm -rf hardware/lineage/compat/Android.bp
 echo -e "${color}Switch back to legacy imsrcsd sepolicy${end}"
 rm -rf device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/ims/imsservice.te
 cp device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/legacy-ims/hal_rcsservice.te device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/ims/hal_rcsservice.te
-
-# Vendor & Kernel Sources
-echo -e "${color}Cloning vendor & kernel from tanvirr007${end}"
-git clone https://github.com/spes-development/vendor_xiaomi_spes -b 14.0 vendor/xiaomi/spes
-git clone https://github.com/spes-development/kernel_xiaomi_sm6225 --depth=1 -b NaughtySilver kernel/xiaomi/sm6225
-
-# Lineage-21 Hardware Source
-echo -e "${color}Cloning Hardware from Lineage-21${end}"
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
